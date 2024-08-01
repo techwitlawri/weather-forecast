e286bbb97c71d0543e001d264470040dbe849324
Mmachi Ezeh <mmachilawri@gmail.com>
2024-08-01 21:19:50 +0100

    added try and except

--- main.py
+++ main.py
@@ -12,20 +12,24 @@ st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
 #  GET THE TEMPERATURE/sky DATA
-    filtered_data = get_data(place,days)
+    try: 
 
-    if option == "Temperature":
-        temperature =[dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # importing plotly, adding graphy
-        # CREATE A TEMEPERATURE PLOT
-        figure = px.line(x= dates, y= temperature, labels={"x":"Date","y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        filtered_data = get_data(place,days)
 
-    if option == "Sky":
-        images= {"Clear": "images/clear.png", "Clouds": "images/cloud.png", 
-                 "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        print(sky_conditions)
-        st.image(image_paths, width= 115) 
+        if option == "Temperature":
+            temperature =[dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # importing plotly, adding graphy
+            # CREATE A TEMEPERATURE PLOT
+            figure = px.line(x= dates, y= temperature, labels={"x":"Date","y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images= {"Clear": "images/clear.png", "Clouds": "images/cloud.png", 
+                    "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width= 115) 
+    except KeyError: 
+        st.write("That place don't exist")
