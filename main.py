25c9aaba01916c5fd43a9b40bc9dfdfecb72eb4c
Mmachi Ezeh <mmachilawri@gmail.com>
2024-08-01 15:31:23 +0100

    Added some functions,and API

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -10,12 +10,7 @@ option= st.selectbox("select date to view",
                       ("Temperature", "Sky")) 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2022-25-10","2022-26-10","2022-27-10"]
-    temperature= [10, 11, 15]   
-#  connecting the temperature with the widget
-    temperature= [days * i for i in temperature]
-    return  dates,temperature
+data = get_data(place, days, option)
 
 d, t = get_data(days)
 # importing plotly, adding graphy
